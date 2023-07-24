1a1b12ef810f0a07ffaaf0641880104fcd27a379
Levi Hutchins <levihutchins0@gmail.com>
2023-07-24 16:05:27 +1000

    model working / api issues

--- backend/model/passwordClassifer.py
+++ backend/model/passwordClassifer.py
@@ -10,8 +10,8 @@ class Classifer:
         self.vectorise = None
 
     def load_model(self):
-        self.vectorise = joblib.load("vectorise.pkl")
-        self.clf = joblib.load("model.pkl")
+        self.vectorise = joblib.load("./pkl/vectorise.pkl")
+        self.clf = joblib.load("./pkl/model.pkl")
 
     def classify_strength(self, password):
         single_prediction = [password]
