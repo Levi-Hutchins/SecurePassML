4e801e79a35b3d1b7d5424e3b79ec0dc705b985f
Levi Hutchins <levihutchins0@gmail.com>
2023-12-06 16:11:01 +1100

    tidy up model and front end dev

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -7,7 +7,16 @@ const Home = () => {
     <div className="Home">
       <header className="App-header">
         <img src={securepass} className="App-logo" alt="logo" />
-        <p className="info"></p>
+        <div className="frontpage-info">
+          Welcome to SecurePassML ! Created by Levi Hutchins this machine
+          learning based web application is intended to give insight into the
+          safety of your passwords. Simply enter your password and view the
+          metrics and information that is generated from your password.
+          This is an open-source project so please feel free to contribute{" "}
+          <a href="https://github.com/Levi-Hutchins/SecurePassML" target="_blank" rel="noopener noreferrer">here</a>.
+
+        </div>
+
         <InputBox />
       </header>
       <div className="AppCore"></div>
