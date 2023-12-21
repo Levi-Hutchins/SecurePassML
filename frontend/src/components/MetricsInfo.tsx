93e593414dc60288b7fdf6e7bde741a6cdbdf419
Levi Hutchins <levihutchins0@gmail.com>
2023-12-21 15:43:07 +1100

    entropy alert component

--- frontend/src/components/MetricsInfo.tsx
+++ frontend/src/components/MetricsInfo.tsx
@@ -1,21 +1,20 @@
 // MetricsInfo.jsx
 import React, { useEffect, useState } from "react";
-import { Alert } from "@mui/material";
+import { Alert, AlertTitle } from "@mui/material";
 import { MetricsInfoProps } from "./Types";
 import { findPasswordComplexity } from "../utils";
 import { findPasswordChars } from "../utils";
 
-
-
 const MetricsInfo: React.FC<MetricsInfoProps> = ({
-
   password,
   passwordLength,
 }) => {
-  const [passwordEntropy, setEntropy] = useState('');
-  useEffect(() =>{
-    setEntropy(Math.log2(Math.pow(findPasswordComplexity(password),passwordLength)).toString());
-  })
+  const [passwordEntropy, setEntropy] = useState(Number);
+  useEffect(() => {
+    setEntropy(
+      Math.log2(Math.pow(findPasswordComplexity(password), passwordLength))
+    );
+  });
   return (
     <div className="panel">
       <h2 className="heading">Metrics</h2>
@@ -38,7 +37,7 @@ const MetricsInfo: React.FC<MetricsInfoProps> = ({
           predictive methods.
         </p>
       </div>
-      <br/>
+      <br />
       {/*
         Weak entropy = < 40
         Meidum entropy = 40 - 60
@@ -48,19 +47,59 @@ const MetricsInfo: React.FC<MetricsInfoProps> = ({
       
       */}
       <div>
-      <Alert
-        severity="success"
-        style={{ backgroundColor: "rgba(76, 175, 80, 0.1)", 
-        height:"50px",
-        borderRadius: "20px",
-        border: "0.5px solid #00ff62ce",
-        color: "#e7e7e7",
-        textAlign: "center"
-
-      }}
-      >
-        Entropy: {passwordEntropy.toString()}
-      </Alert>
+        {passwordEntropy > 60 ? (
+          <div>
+            <Alert
+              severity="success"
+              style={{
+                backgroundColor: "rgba(76, 175, 80, 0.1)",
+                height: "50px",
+                borderRadius: "20px",
+                border: "0.5px solid #00ff62ce",
+                color: "#e7e7e7",
+                textAlign: "center",
+              }}
+            >
+              Good Entropy: {Math.round(passwordEntropy).toString()} bits
+            </Alert>
+          </div>
+        ) : (
+          <div>
+            {passwordEntropy < 60 && passwordEntropy > 40 ? (
+              <div>
+                <Alert
+                  severity="warning"
+                  style={{
+                    backgroundColor: "rgba(255, 193, 7, 0.1)", // Orange-yellow shade with transparency
+                    height: "50px",
+                    borderRadius: "20px",
+                    border: "0.5px solid #ffeb3b", // Bright yellow border
+                    color: "#e7e7e7",
+                    textAlign: "center",
+                  }}
+                >
+                  Medium Entropy: {Math.round(passwordEntropy).toString()} bits
+                </Alert>
+              </div>
+            ) : (
+              <div>
+                <Alert
+                  severity="error"
+                  style={{
+                    backgroundColor: "rgba(211, 47, 47, 0.1)", // Semi-transparent red background
+                    height: "50px",
+                    borderRadius: "20px",
+                    border: "0.5px solid #ff1744", // Bright red border
+                    color: "#e7e7e7",
+                    textAlign: "center",
+                  }}
+                >
+                  Weak Entropy: {Math.round(passwordEntropy).toString()} bits
+                </Alert>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
